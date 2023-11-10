14ff6c5b57905ecf1708f21dd9af63b89f046eb3
Tanner Ghosen <tannerghosen@yahoo.com>
2023-11-10 14:52:51 -0500

    Fixed issue causing an invalid padding error.Replaced Base64 encryption/decryption with AES encryption/decryption using a mixture of the example given by Microsoft and tutorials.

--- Wasspord.cs
+++ Wasspord.cs
@@ -9,6 +9,7 @@ using System.Collections;
 using System.IO.Pipes;
 using System.Security.Cryptography;
 
+
 namespace Wasspord
 {
     /*
@@ -29,10 +30,12 @@ namespace Wasspord
             public string where;
             public string username;
         }
-		/* AddAccount: Adds an account with a location *where* it's used, the username and the encrypted password, to above mentioned
+        private static string Key = "p055w4rd";
+        private static byte[] Bytes = { 0x31, 0xab, 0xa7, 0x91, 0x93, 0x9b, 0x7d, 0x1f, 0x3b, 0xf7, 0x8d, 0x3f, 0x9a };
+        /* AddAccount: Adds an account with a location *where* it's used, the username and the encrypted password, to above mentioned
 		   account dictionary, which can be saved to a .wasspord file down the line.
          * Parameters: where (location), username, password. */
-		public static void AddAccount(string where, string username, string password)
+        public static void AddAccount(string where, string username, string password)
         {
             Account acc;
             acc.where = where;
@@ -146,25 +149,70 @@ namespace Wasspord
             }
             return display;
         }
-		/* Encrypt: Encrypts the account password before it's saved to the account dictionary in Base64. Can be decrypted by Decrypt.
+
+        /* Encrypt: Encrypts the account password before it's saved to the account dictionary using AES. Can be decrypted by Decrypt.
 		 * Parameters: password 
 		 * Returns: encrypted password */
-		private static string Encrypt(string password)
+
+        public static string Encrypt(string password)
         {
-            byte[] b = ASCIIEncoding.ASCII.GetBytes(password);
-            string encrypted = Convert.ToBase64String(b);
-            return encrypted;
+            // Get bytes from password
+            byte[] b = Encoding.Unicode.GetBytes(password);
+
+            // Create Aes object
+            using (Aes encrypt = Aes.Create())
+            {
+                // Derieve bytes from Key and Bytes
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(Key, Bytes);
+                encrypt.Key = pdb.GetBytes(32); 
+                encrypt.IV = pdb.GetBytes(16);
+                // Create streams for encryption
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encrypt.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(b, 0, b.Length);
+                        cs.Close();
+                    }
+                    // Password is encrypted as a Base64 String based on the content from above Streams.
+                    password = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return password;
+
         }
-		/* Decrypt: Decrypts the account password previously encrypted and saved to either an account dictionary or a .wasspord file.
+        /* Decrypt: Decrypts the account password previously encrypted and saved to either an account dictionary or a .wasspord file.
 		 * Parameters: password
 		 * Returns: decrypted password */
-		private static string Decrypt(string password)
+        public static string Decrypt(string password)
         {
-            byte[] b;
-            string decrypted;
-            b = Convert.FromBase64String(password);
-            decrypted = ASCIIEncoding.ASCII.GetString(b);
-            return decrypted; 
+            // This is in case passwords had a space in them (like "test world"), it would error out without this.
+            password = password.Replace(" ", "+");
+
+            // Get bytes from password
+            byte[] b = Convert.FromBase64String(password);
+
+            // Create Aes object
+            using (Aes encrypt = Aes.Create())
+            {
+                // Derieve bytes from Key and Bytes
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(Key, Bytes);
+                encrypt.Key = pdb.GetBytes(32);
+                encrypt.IV = pdb.GetBytes(16);
+
+                // Create streams for encryption
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encrypt.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(b, 0, b.Length);
+                        cs.Close();
+                    }
+                    // Password is converted to a regular Unicode String based on contents from above stream.
+                    password = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return password;
         }
         /* GeneratePassword: Generates a random password that's 16 characters in length.
          * Returns: Generated Password */
