0e1b2e62c1eafada5a9b60b518f77ac3bf842021
Tanner Ghosen <tannerghosen@yahoo.com>
2024-09-24 20:54:49 -0400

    Last update tonight for real this time

--- WasspordTests/Properties/AssemblyInfo.cs
+++ WasspordTests/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("64cfaa64-0cac-4d56-87d2-b901c269f931")]
+[assembly: Guid("9085ce06-0cf7-4ce3-95ce-9b3127262c2a")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
