f231a4105fd30be66c0cdb40f82728e65c0e2d56
Tanner Ghosen <tannerghosen@yahoo.com>
2024-09-24 21:40:05 -0400

    Updated Logger to have flavor texts for error / debug. Tweaked workflow for the final final FINAL time tonight.

--- Logger.cs
+++ Logger.cs
@@ -18,17 +18,18 @@ namespace Wasspord
         /* Write: Writes a message to our Wasspord.log, usually important info such as errors, warnings, or debug info I'd appreciate if an issue arises. */
         public static void Write(string message, string messagetype = "LOG")
         {
+            string[] flavortexts = { "The problem probably lies in", "The stack is as follows:" };
             string Time = DateTime.Now.ToString("M/d/yyyy h:mm:ss tt");
+            StackTrace st = new StackTrace(); // Create a stack trace
+            StackFrame parentsf = st.GetFrame(1); // this is the parent of the method call
+            StackFrame grandparentsf = st.GetFrame(2); // this is the grandparent (parent's parent) of the method call
+            string stack = grandparentsf.GetMethod().Name + " -> " + parentsf.GetMethod().Name; // this is a string that says Grandparent -> Parent
             using (StreamWriter writer = new StreamWriter(Log, true))
             {
                 writer.WriteLine("(" + Time + ") [" + messagetype + "]: " + message);
                 if (messagetype == "ERROR" || messagetype == "DEBUG") // if error, let's help out by giving the stack trace
                 {
-                    StackTrace st = new StackTrace(); // Create a stack trace
-                    StackFrame parentsf = st.GetFrame(1); // this is the parent of the method call
-                    StackFrame grandparentsf = st.GetFrame(2); // this is the grandparent (parent's parent) of the method call
-                    string stack = grandparentsf.GetMethod().Name + " -> " + parentsf.GetMethod().Name; // this is a string that says Grandparent -> Parent
-                    writer.WriteLine("(" + Time + ") [" + messagetype + "]: The problem probably lies in " + stack + ".");
+                    writer.WriteLine("(" + Time + ") [" + messagetype + "]: " + messagetype == "ERROR" ? flavortexts[0] : flavortexts[1] + " " + stack + ".");
                 }
                 writer.Close();
             }
